3f1b6a497db1407449a0bceb658ff507a157865f
Tanner Hoke <tanner_h@me.com>
2015-04-05 10:02:58 -0500

    Updated comments.

--- src/main/PaintPanel.java
+++ src/main/PaintPanel.java
@@ -12,20 +12,23 @@ import main.PaintSurface.ImplementedShape;
 
 public class PaintPanel extends JPanel {
 
+	// Shape choosing buttons.
 	private JButton rectangleBtn, ellipseBtn;
+	
 	private PaintSurface surface;
 	
 	public PaintPanel() {
 		super();
 		this.setLayout(new BorderLayout());
-
 		
 		surface = new PaintSurface();
 		add(surface);
 		
+		// Create buttons.
 		ellipseBtn = new JButton("Ellipse");
 		rectangleBtn = new JButton("Rectangle");
 		
+		// Add action listeners to the buttons.
 		ellipseBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				surface.setCurrentShape(ImplementedShape.Ellipse);
@@ -38,6 +41,8 @@ public class PaintPanel extends JPanel {
 			}
 		});
 		
+		// Add buttons. Probably should make this a bit prettier than
+		// one button on the left and one on the right, but it works.
 		add(BorderLayout.WEST, rectangleBtn);
 		add(BorderLayout.EAST, ellipseBtn);
 	}
